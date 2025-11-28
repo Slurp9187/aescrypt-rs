7bdf68e06b4e9ea9eb74c7626078e81ce0c6b271
Slurp9187 <Slurp9187@users.noreply.github.com>
2025-11-28 06:00:54 -0700

    Cleanup Cargo.toml

--- src/decryptor/stream/mod.rs
+++ src/decryptor/stream/mod.rs
@@ -1,5 +1,6 @@
-mod context;
-mod utils;
-mod versions;
+// src/decryptor/stream/mod.rs
+pub(crate) mod context;
+pub(crate) mod utils;
+pub(crate) mod versions;
 
 pub use versions::{decrypt_ciphertext_stream, StreamConfig};
